d7429d1e8edb2569d5a43338ff4294e56f4e3098
sumedh-karwande <sumedhkarvande18@gmail.com>
2023-02-14 23:03:26 +0530

    GPU  api create

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,9 +2,8 @@
 
 const Product = require("../models/productModel");
 
-
-// create product 
-	exports.createProduct = async (req, res, next) => {
+// create product --admin--
+exports.createProduct = async (req, res, next) => {
 	const product = await Product.create(req.body);
 	res.status(201).json({
 		sucess: true,
@@ -12,7 +11,64 @@ const Product = require("../models/productModel");
 	});
 };
 
-exports.getAllProducts = (req, res) => {
-	res.status(200).json({ message: "Route is working fine" });
+// get all product --admin--
+exports.getAllProducts = async (req, res, next) => {
+	const product = await Product.find();
+	res.status(200).json({
+		sucess: true,
+		product,
+	});
 };
 
+// update product  --admin update--
+exports.updateProduct = async (req, res, next) => {
+	let product = await Product.findById(req.params.id);
+	if (!product) {
+		return res.status(500).json({
+			sucess: false,
+			message: "Product not found",
+		});
+	}
+	product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+		useFindAndModify: false,
+	});
+	res.status(200).json({
+		sucess:true ,
+		product ,
+	})
+};
+
+// Get product details
+exports.getProductDetails = async(req ,res ,next) => {
+	const product = await Product.findById(req.params.id);
+	if(!product){
+		res.status(500).json({
+			sucess :false ,
+			message :'Product not found'
+		})
+	}
+	
+	res.status(200).json({
+		sucess :true ,
+		product 
+	})
+}
+
+// Delete Product ---admin---
+exports.deleteProduct = async (req ,res ,next) =>{
+	const product = await Product.findById(req.params.id);
+	if(!product){
+		return res.status(500).json({
+			sucess :false ,
+			message:'Product not found'
+		})
+		
+	}
+	await product.remove();
+	res.status(200).json({
+		sucess :true ,
+		message:'Product Deleted Successfully'
+	})
+}
