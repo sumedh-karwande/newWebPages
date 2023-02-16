5a3f52b9fc16ad1d0632b9c1e324200bc2e32fa5
sumedh-karwande <sumedhkarvande18@gmail.com>
2023-02-16 23:48:44 +0530

    filter, pagination

--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -4,6 +4,7 @@ class ApiFeatures {
         this.query =query;
         this.queryStr =queryStr;
     }
+    
 
     search() {
         const keyword = this.queryStr.keyword ?{
@@ -12,11 +13,37 @@ class ApiFeatures {
                 // $option :`i`,
             }
         } :{}
-        console.log(keyword);
         this.query = this.query.find({...keyword});
         return this;
     }
 
+
+    filter() {
+        const queryCopy = {...this.queryStr};
+         // console.log(queryCopy);
+        // removing some field for Category
+        const removeField = ["keyword" ,"page" ,"limit"];
+        removeField.forEach(key=>delete queryCopy[key]);
+        // console.log(queryCopy)
+        
+    // Filter For Price and Rating
+
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+        return this ;
+    }
+
+
+    pagination(resultPerPage) {
+        const currentPage = Number(this.queryStr.page) || 1;
+    
+        const skip = resultPerPage * (currentPage - 1);
+    
+        this.query = this.query.limit(resultPerPage).skip(skip);
+    
+        return this;
+      }
 }
 
 module.exports = ApiFeatures;
